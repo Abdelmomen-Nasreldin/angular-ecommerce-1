6a8bf088dea673c50498adbd009f216846bea41d
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2023-09-15 18:27:57 +0300

    setup the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProductComponent } from './Components/product/product.component';
+import { ProductsListComponent } from './Components/products-list/products-list.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { CarouselComponent } from './Components/carousel/carousel.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    ProductsListComponent,
+    ProductDetailsComponent,
+    NotFoundComponent,
+    CarouselComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
